eebca1fc77a11b33fcd87ec303b1acb6887a1f03
Simone <150595274+simonetimi@users.noreply.github.com>
2025-02-11 20:53:30 +0100

    fix heroui config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,17 +3,11 @@ import { heroui } from "@heroui/react";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   darkMode: "class",
   plugins: [heroui()],
