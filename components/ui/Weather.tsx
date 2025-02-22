d9fcf2b28094d638d0351e2c05ba49bc82d980e3
Simone <150595274+simonetimi@users.noreply.github.com>
2025-02-22 13:15:48 +0100

    integrate translations

--- components/ui/Weather.tsx
+++ components/ui/Weather.tsx
@@ -7,18 +7,20 @@ import { Thermometer } from "lucide-react";
 import { Tooltip } from "@heroui/tooltip";
 import { capitalize, fromCamelCase } from "@/utils/strings";
 import { cloneElement } from "react";
+import { useTranslations } from "next-intl";
 
 export default function Weather({ weather }: { weather: WeatherForecasts }) {
   const svgProps = { width: 20, height: 20 };
+  const t = useTranslations("game");
   return (
     <div className="flex items-center justify-evenly text-sm">
-      <Tooltip content={capitalize(weather.condition)}>
+      <Tooltip content={capitalize(t("weather." + weather.condition))}>
         {ForecastMap[weather.condition as keyof typeof ForecastMap]}
       </Tooltip>
       <span className="flex items-center">
         <Thermometer /> {weather.temperature}°C
       </span>
-      <Tooltip content={fromCamelCase(weather.lunarPhase)}>
+      <Tooltip content={fromCamelCase(t("moon." + weather.lunarPhase))}>
         {cloneElement(
           MoonMap[weather.lunarPhase as keyof typeof MoonMap],
           svgProps,
