09949325094228e2ad2e04992eb9f6a5c8c81a3f
Simone <150595274+simonetimi@users.noreply.github.com>
2025-03-04 19:53:52 +0100

    improve ui for login buttons

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -16,33 +16,37 @@ export default function Login() {
   };
 
   return (
-    <div className="flex gap-2">
+    <div className="flex w-36 flex-col gap-3">
       <Button
         isLoading={loadingProvider === "github"}
         disabled={loadingProvider !== null}
-        isIconOnly
         color="primary"
         variant="light"
+        className="bg-default/60 text-foreground"
         startContent={
           loadingProvider !== "github" && (
             <Github className="h-5 w-5 dark:fill-white" />
           )
         }
         onPress={() => handleLogin("github")}
-      />
+      >
+        Github
+      </Button>
       <Button
         isLoading={loadingProvider === "google"}
         disabled={loadingProvider !== null}
-        isIconOnly
         color="primary"
         variant="light"
+        className="bg-default/60 text-foreground"
         startContent={
           loadingProvider !== "google" && (
             <Google className="h-5 w-5 rounded-full" />
           )
         }
         onPress={() => handleLogin("google")}
-      />
+      >
+        Google
+      </Button>
     </div>
   );
 }
